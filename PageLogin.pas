39eb833db728cefd060554725d0b8194e69ff036
Mott laulha <mottlaulha@gmail.com>
2020-07-16 16:57:41 +0200

    Squelette de la page fait login fait

--- PageLogin.pas
+++ PageLogin.pas
@@ -4,10 +4,24 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, FMX.Objects, FMX.Edit;
 
 type
   TForm1 = class(TForm)
+    bgcLogin: TImage;
+    centerElement: TLayout;
+    headerCenterElement: TLayout;
+    imgLogo: TImage;
+    Label1: TLabel;
+    imgUser: TImage;
+    userContainer: TLayout;
+    Line1: TLine;
+    Label2: TLabel;
+    userName: TLabel;
+    Label3: TLabel;
+    LineOne: TLine;
+    Edit1: TEdit;
   private
     { Déclarations privées }
   public
